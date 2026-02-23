2c075e6bbc4f17dbe1fbcf19c25eeb0b5fa0d369
eduromero10 <eduromeroafonso04@gmail.com>
2026-02-23 03:47:11 +0000

    nuevo cambio

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdminPageRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard.component';
@@ -20,6 +20,7 @@ import { ConfirmationModalComponent } from './confirmation-modal/confirmation-mo
     CommonModule,
     IonicModule,
     AdminPageRoutingModule,
+    FormsModule,
     ReactiveFormsModule,
   ],
 })
